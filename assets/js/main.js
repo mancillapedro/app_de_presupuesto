0e737ee9411984cb3da11e8dce2e966e2e05a8e0
_ <pedropmancillac@gmail.com>
2023-04-22 19:39:50 -0400

    formExpense

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,58 @@
-
 const formBudget = () => {
     const
         form = document.querySelector('#formBudget'),
-        inputBudget = form.querySelector('#inputBudget');
-    buttonSubmit = form.querySelector('[type="submit"]');
+        inputBudget = form.querySelector('#inputBudget'),
+        buttonSubmit = form.querySelector('[type="submit"]')
     return {
         events: ({ outputBudget }) => {
             inputBudget.addEventListener('input', ({ target }) => {
-                target.value = target.value.replace(/[^0-9]/g, '');
-                buttonSubmit.disabled = Number(target.value) < 1;
-                console.log(buttonSubmit, typeof target.value)
+                target.value = target.value.replace(/[^0-9]/g, '')
+                buttonSubmit.disabled = Number(target.value) < 1
             })
-            form.addEventListener('submit', (e) => {
-                e.preventDefault()
+            form.addEventListener('submit', ({ preventDefault }) => {
+                preventDefault()
                 outputBudget.innerText = inputBudget.value;
+                form.reset()
             })
         }
     }
 }
 
+const formExpense = () => {
+    const
+        form = document.querySelector('#formExpense'),
+        inputAmountExpense = form.querySelector('#amountExpense'),
+        inputNameExpense = form.querySelector('#nameExpense'),
+        buttonSubmit = form.querySelector('[type="submit"]'),
+        disabledSubmit = () =>
+            buttonSubmit.disabled = !Boolean(inputNameExpense.value) || Number(inputAmountExpense.value) < 1
+    return {
+        events: () => {
+            inputAmountExpense.addEventListener('input', disabledSubmit)
+            inputAmountExpense.addEventListener('keypress',
+                ({ key, preventDefault }) =>
+                    (key == " " || isNaN(key)) && preventDefault()
+            )
+            inputNameExpense.addEventListener('input', disabledSubmit)
+            inputNameExpense.addEventListener('keypress',
+                ({ keyCode, preventDefault }) => keyCode == 13 && preventDefault()
+            )
+            form.addEventListener('submit',
+                ({ preventDefault }) => {
+                    preventDefault()
+                    // TODO: output 
+                    form.reset()
+                }
+            )
+        }
+    }
+}
+
+
+
 addEventListener('DOMContentLoaded', () => {
     const outputBudget = document.querySelector('#outputBudget');
-
+    const outputExpense = document.querySelector('#outputExpense');
     formBudget().events({ outputBudget })
-
-
+    formExpense().events({ outputExpense })
 })
